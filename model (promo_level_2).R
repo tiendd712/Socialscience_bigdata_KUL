567643f4f33fc92cc54c3850001ee09ad46604f5
aphdinh <56706731+aphdinh@users.noreply.github.com>
2023-07-21 17:58:30 +0700

    Update model (promo_level_2).R

--- model (promo_level_2).R
+++ model (promo_level_2).R
@@ -15,8 +15,11 @@ exp_data = read_csv("data_model.csv")
 
 
 # Preprocess the data
+language_columns <- c("English", "French", "Dutch", "German", "Spanish", "Hindi", "Chinese")
+
 df <- exp_data %>%
   merge(edu_data, by = "employee_id", all.x = TRUE) %>%
+  filter(time_work > 0) %>% 
   merge(lang_data, by = "employee_id", all.x = TRUE) %>%
   merge(connection_data, by = "employee_id", all.x = TRUE) %>%
   merge(follower_data, by = "employee_id", all.x = TRUE) %>%
@@ -29,7 +32,11 @@ df <- exp_data %>%
   replace(is.na(.), 0) %>%
   mutate(promote_level_2 = factor(promote_level_2, levels = c(0, 1), labels = c(0, 1)),
          gender_predict = factor(gender_predict, levels = c("Neutral", "Man", "Woman"), labels = c(0,1,2)),
-         highest_edu = factor(highest_edu, levels = c("bachelor", "master", "phd"), labels=c(0,1,2)))
+         highest_edu = factor(highest_edu, levels = c("bachelor", "master", "phd"), labels=c(0,1,2)),
+         has_relevant_field = factor(has_relevant_field, levels = c(0, 1), labels = c(0, 1)),
+         intern = factor(intern, levels = c(0, 1), labels = c(0, 1))) %>% 
+  mutate_at(vars(all_of(language_columns)), factor,
+            levels = c(0, 1))
 
 dim(df)
 
@@ -53,28 +60,29 @@ y_train <- as.factor(data_train$promote_level_2)
 x_test <- data_test %>% select(-promote_level_2)
 y_test <- as.factor(data_test$promote_level_2)
 
-print(levels(y_train))
-print(levels(y_test))
-
 # SMOTE
 data_smote <- data_train
 data_smote$promote_level_2 <- as.numeric(data_smote$promote_level_2)
 data_smote$highest_edu <- as.numeric(data_smote$highest_edu)
 data_smote$gender_predict <- as.numeric(data_smote$gender_predict)
-smote_train <- SMOTE(data_train, target=data_train$promote_level_2, K=5, dup_size = 0)
-smote_train$promote_level_2 <- as.factor(smote_train$promote_level_2)
-smote_train$highest_edu <- as.factor(smote_train$highest_edu)
-smote_train$gender_predict <- as.factor(smote_train$gender_predict)
+data_smote <- SMOTE(data_smote, target=data_smote$promote_level_2, K=10, dup_size = 0)
+data_smote$promote_level_2 <- as.factor(smote_train$promote_level_2)
+data_smote$highest_edu <- as.factor(smote_train$highest_edu)
+data_smote$gender_predict <- as.factor(smote_train$gender_predict)
+gen_data_df <- as.data.frame(data_smote)
+
 
 # Model
 rf_model <- randomForest(x_train, y_train, ntree = 100)
-# rf_model_smote <- randomForest(smote_train, y_train, ntree = 100)
+
+rf_model <- randomForest(promote_level_2 ~ ., data = data_smote, ntree = 100)
 print(rf_model)
 
 # Predict
 rf_predictions <- predict(rf_model, x_test)
 sum(rf_predictions == 0)/length(rf_predictions) * 100
 sum(rf_predictions == 1)/length(rf_predictions) * 100
+shap_values=predict(rf_model, x_test, predcontrib = TRUE, approxcontrib = F)
 
 # Evaluate
 conf_matrix <- confusionMatrix(rf_predictions, y_test, positive='1')
@@ -86,5 +94,8 @@ model <- glm(promote_level_2 ~ .,data=data_train, family=binomial(link = "logit"
 model_summary <- summary(model)
 print(model_summary)
 pred <- predict(model, data_test, type="response")
-c_matrix <- confusionMatrix(data= as.factor(as.numeric(pred>0.3)), reference = data_test$promote_level_2, positive = "1")
+c_matrix <- confusionMatrix(data= as.factor(as.numeric(pred>0.5)), reference = data_test$promote_level_2, positive = "1")
 c_matrix
+c_matrix$byClass
+ 
+

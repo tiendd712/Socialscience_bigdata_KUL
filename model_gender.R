98b29873f8e94941767835c484230d5fa5bc0b09
aphdinh <56706731+aphdinh@users.noreply.github.com>
2023-07-21 09:12:54 +0700

    Update model_gender.R

--- model_gender.R
+++ model_gender.R
@@ -40,21 +40,23 @@ prop.table(table(data_train$gender_label))
 prop.table(table(data_test$gender_label))
 
 # Prepare data
-x <- data_train %>% select(-gender_label)
-y <- as.factor(data_train$gender_label)
+x_train <- data_train %>% select(-gender_label)
+y_train <- as.factor(data_train$gender_label)
+x_test <- data_test %>% select(-gender_label)
+y_test <- as.factor(data_test$gender_label)
 
 # Model
-rf_model <- randomForest(x, y, ntree = 100)
+rf_model <- randomForest(x_train, y_train, ntree = 100)
 print(rf_model)
 
 # Predict
-rf_predictions <- predict(rf_model, x)
+rf_predictions <- predict(rf_model, x_test)
 percent_woman <- sum(rf_predictions == 1)/length(rf_predictions) * 100
 percent_man <- sum(rf_predictions == 2)/length(rf_predictions) * 100
 
 # Evaluate
 # Extracting evaluation metrics for woman (label 1)
-conf_matrix <- confusionMatrix(rf_predictions, y)
+conf_matrix <- confusionMatrix(rf_predictions, y_test)
 conf_matrix 
 f1_score_woman <- conf_matrix$byClass['F1'][1]
 recall_woman <- conf_matrix$byClass['Sensitivity'][1]
@@ -64,4 +66,48 @@ precision_woman <- conf_matrix$byClass['Pos Pred Value'][1]
 cat("F1 Score for Woman:", f1_score_woman, "\n")
 cat("Recall for Woman:", recall_woman, "\n")
 cat("Precision for Woman:", precision_woman, "\n")
+
+# Define the hyperparameter grid
+param_grid <- expand.grid(
+  ntree = c(50, 100, 150),
+  mtry = c(4, 6, 8),
+  max_depth = c(10, 20, 30)
+)
+
+# Initialize variables to store best model and performance
+best_model <- NULL
+best_f1_score <- 0
+
+# Perform grid search
+for (i in 1:nrow(param_grid)) {
+  rf_model <- randomForest(
+    x_train, y_train,
+    ntree = param_grid$ntree[i],
+    mtry = param_grid$mtry[i],
+    max_depth = param_grid$max_depth[i]
+  )
+  
+  rf_predictions <- predict(rf_model, x_test)
+  conf_matrix <- confusionMatrix(rf_predictions, y_test)
+  f1_score_woman <- conf_matrix$byClass['F1'][1]
+  
+  if (f1_score_woman > best_f1_score) {
+    best_model <- rf_model
+    best_f1_score <- f1_score_woman
+  }
+}
+
+# Evaluate the best model
+best_rf_predictions <- predict(best_model, x_test)
+conf_matrix <- confusionMatrix(best_rf_predictions, y_test)
+f1_score_woman <- conf_matrix$byClass['F1'][1]
+recall_woman <- conf_matrix$byClass['Sensitivity'][1]
+precision_woman <- conf_matrix$byClass['Pos Pred Value'][1]
+
+# Print evaluation metrics for woman
+conf_matrix
+cat("Best F1 Score for Woman:", f1_score_woman, "\n")
+cat("Best Recall for Woman:", recall_woman, "\n")
+cat("Best Precision for Woman:", precision_woman, "\n")
+
 

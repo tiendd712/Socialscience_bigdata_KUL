97b8b7a48f76227980b186aef5d0a8896e8b5673
tiendd712 <doductien0712@gmail.com>
2023-07-27 18:09:35 +0200

    Tien_update

--- model_promo_level1.R
+++ model_promo_level1.R
@@ -69,12 +69,15 @@ print(levels(y_train_level_1))
 print(levels(y_test_level_1))
 
 
+write.csv(df_level_1, "C:/Users/doduc/df_level_1.csv", row.names = F)
+
+write.csv(train_indices, "C:/Users/doduc/train_indices.csv", row.names = F)
 # ================================ Logistics model ==============================
 
 model_level_1 <- glm(promote_level_1 ~ .,data=data_train_level_1, family=binomial(link = "logit"))
 summary(model_level_1)
 
-# =============================== Random forest model ===========================
+# =============================== XGboost model ===========================
 
 ## One hot encoding gender and highest_edu  
 
@@ -82,10 +85,6 @@ df_level_1_e = dummy_cols(df_level_1, select_columns = c("highest_edu", "gender_
                           remove_selected_columns = T) 
 
 
-
-for (i in c(24:29)){
-  df_level_1_e[,i] = as.factor(df_level_1_e[,i])
-  }
 
 data_train_level_1_e <- df_level_1_e[train_indices, ]
 data_test_level_1_e <- df_level_1_e[-train_indices, ]
@@ -96,156 +95,107 @@ x_train_level_1_e <- data_train_level_1_e %>% select(-promote_level_1)
 
 x_test_level_1_e <- data_test_level_1_e %>% select(-promote_level_1)
 
-## Tunning hyperparameter for random forest
 
-# Define the hyperparameter grid
-param_grid <- expand.grid(
-  ntree = c(50, 100, 150),
-  mtry = c(4, 6, 8),
-  max_depth = c(10, 20, 30)
-)
 
-# Initialize variables to store best model and performance
-best_model <- NULL
-best_f1_score <- 0
+## XGboost model need numeric input
 
-# Perform grid search
-for (i in 1:nrow(param_grid)) {
-  rf_model <- randomForest(
-    x_train_level_1_e, y_train_level_1,
-    ntree = param_grid$ntree[i],
-    mtry = param_grid$mtry[i],
-    max_depth = param_grid$max_depth[i]
-  )
-  
-  rf_predictions <- predict(rf_model, x_test_level_1_e)
-  conf_matrix <- confusionMatrix(rf_predictions, y_test_level_1)
-  f1_score <- conf_matrix$byClass['F1'][1]
-  
-  if (f1_score > best_f1_score) {
-    best_model <- rf_model
-    best_f1_score <- f1_score
+
+for (i in c(1:ncol(x_train_level_1_e))){
+  if (is.factor(x_train_level_1_e[,i])){
+    x_train_level_1_e[,i] = as.numeric(x_train_level_1_e[,i]) - 1
   }
 }
 
-# Evaluate the best model
-best_rf_predictions <- predict(rf_model, x_test_level_1_e)
-conf_matrix <- confusionMatrix(best_rf_predictions, y_test_level_1, positive='1')
+for (i in c(1:ncol(x_test_level_1_e))){
+  if (is.factor(x_test_level_1_e[,i])){
+    x_test_level_1_e[,i] = as.numeric(x_test_level_1_e[,i]) - 1
+  }
+}
 
 
 
+## Tunning hyperparameter for XGboost
 
-rf_model <- randomForest(
-  x_train_level_1_e, y_train_level_1,
-  ntree = 85,
-  max_depth = 7,
-  nodesize = 1,
-  
+# Define the hyperparameter grid
+
+set.seed(7)
+param_grid <- data.frame(
+  nrounds = sample(50:200, 50),                   
+  max_depth = sample(1:50, 50, replace = T),                    
+  eta = runif(50, 0.01, 0.5),                      
+  colsample_bytree = runif(50, 0.6, 1),
+  min_child_weight = sample(1:10, 50, replace = T),
+  gamma = runif(50, 0, 1),
+  subsample = runif(50, 0.7, 1)
+)
+
+
+# Specify the cross-validation settings
+ctrl <- trainControl(
+  method = "cv",              
+  number = 5,                 
+  search = "random",          
+  verboseIter = TRUE          
 )
 
 
 
+# Perform Randomized Search Cross-Validation using the xgb.train function
+tunning_model <- train(
+  x = as.matrix(x_train_level_1_e),
+  y = y_train_level_1,
+  method = "xgbTree",          # Specifies the xgboost model
+  trControl = ctrl,            # Use the control settings from step 4
+  tuneGrid = param_grid        # Use the hyperparameter grid from step 3
+)
 
+write.csv(tunning_model$bestTune,
+          "C:/Users/doduc/Github/Socialscience_bigdata_KUL/tunning_model_level1.csv",
+          row.names = F)
 
 
+## model
 
+params <- list(
+  objective = "binary:logistic",   
+  eval_metric = "error",         
+  nrounds = tunning_model$bestTune$nrounds,                   
+  max_depth = tunning_model$bestTune$max_depth,
+  eta = tunning_model$bestTune$eta,
+  gamma = tunning_model$bestTune$gamma,
+  colsample_bytree = tunning_model$bestTune$colsample_bytree,
+  min_child_weight = tunning_model$bestTune$min_child_weight,
+  subsample = tunning_model$bestTune$subsample
+  
+)
 
-s <- kernelshap(rf_model, x_train_level_1_e, bg_X = x_train_level_1_e[c(1:500),])
 
-# Step 2: Turn them into a shapviz object
-sv <- shapviz(s)
+# Train the XGBoost model
+xgb_model <- xgboost(data = as.matrix(x_train_level_1_e), 
+                      label =  as.numeric(y_train_level_1) - 1, 
+                      params = params,
+                      nrounds = tunning_model$bestTune$nrounds)
 
 
-shapper::
+# Make prediction
+predictions = predict(model, as.matrix(x_test_level_1_e))
 
 
+predictions <- ifelse(predictions >= 0.5, 1, 0)
 
 
 
+confusionMatrix(factor(predictions), y_test_level_1, positive="1")
+                                                                                                        
 
 
+# SHAP plot
+p = xgboost::xgb.plot.shap.summary(data = as.matrix(x_train_level_1_e),
+                                   model = xgb_model)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+p + ggplot2::scale_colour_viridis_c(limits = c(-2, 4),
+                                    option = "plasma", direction = -1) +
+  labs(y = "SHAP value", x = "Feature") + 
+  theme_bw()
+  
